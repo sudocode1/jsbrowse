156593831c342f5bc1cbd987ac57c5ebcb2a6063
Iris <52894745+sudocode1@users.noreply.github.com>
2020-04-18 19:06:12 +0100

    v1.0.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# jsbrowse
+browse the web using js
+
+## install
+`npm install jsbrowse`
+
+## example usage
+```js
+const jsbrowse = require("./index");
+const browser = new jsbrowse.browse("http://justgone.uk/onebot/api");
+
+browser.browse("index.html");
+```
+
+make sure the file you save to is html (unless you want to read the code, make it txt)
---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const request = require("request");
+
+class browse {
+    constructor(site) {
+        this.site = site;
+    }
+    browse(file) {
+        request(this.site, function(error, response, body) {
+            console.error('error:', error);
+            console.log("statusCode:", response && response.statusCode);
+            console.log("body", body);
+            fs.writeFile(file, body, (err) => {
+                if (err) throw err;
+                console.log("HTML Saved in " + file);
+            });
+        });
+    }
+}
+
+module.exports = { browse };
